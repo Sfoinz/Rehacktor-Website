e5b8ad07908f46e8e90a76cba4bc3d6d44a5ec16
SalvatoreCavallaro <tore1793@gmail.com>
2025-07-06 11:21:11 +0200

    Added .env example, update genrepage with cards foreach genre

--- src/hooks/useApiKey.jsx
+++ src/hooks/useApiKey.jsx
@@ -2,7 +2,7 @@ export default function useApiKey() {
   const apiKey = import.meta.env.VITE_RAWG_API_KEY;
 
   if (!apiKey) {
-    console.warn("⚠️ Nessuna API key trovata in VITE_RAWG_API_KEY");
+    console.warn(" Nessuna API key trovata in VITE_RAWG_API_KEY");
   }
 
   return apiKey;
--- src/pages/genrepage/index.jsx
+++ src/pages/genrepage/index.jsx
@@ -1,8 +1,52 @@
-import { Link } from 'react-router';
+import {useState, useEffect } from "react";
+import { useParams } from "react-router";
+import CardGame from "../../components/CardGame";
+import useApiKey from "../../hooks/useApiKey";
 
 function GenrePage() {
+
+    const apiKey = useApiKey();
+    const { genre } = useParams();
+
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    const url = `https://api.rawg.io/api/games?key=${apiKey}&genres=${genre}&page=1`;
+
+    const load = async () => {
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            const json = await response.json();
+            setData(json);
+
+        } catch (error) {
+            setError(error.message);
+            setData(null);
+        }
+    }
+
+    useEffect(() => {
+        load();
+
+    }, [genre]);
+
+
     return (
-        <p>Pagina dei generi</p>
+        <div>
+            <h2>Welcome to {genre} page</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+                {error && <article>{error}</article>}
+                {data && data.results.map((game) => (
+                    <CardGame key={game.id} game={game}></CardGame>
+                ))}
+
+
+            </div>
+        </div>
+
     )
 }
 
